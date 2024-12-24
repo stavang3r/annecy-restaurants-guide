68703cc08e8daf2d62d083b80120a18c032f0cf6
stavang3r <julian.festor@gmail.com>
2024-12-24 06:43:44 +0100

    feat: Add restaurant guide component with filters and map

---
+++ src/components/RestaurantGuide.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { MapPin, Clock, Phone, Globe, Star, Search, Filter } from 'lucide-react';
+
+// ... [Rest of the component code would go here]
+
+export default RestaurantGuide;
